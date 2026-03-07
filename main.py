5fde07572f26f105a149bb80e544c77f2348a3f9
BoraAbramov <borya812@gmail.com>
2026-03-07 21:21:06 +0200

    update...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 def create_board():
+    #יוצר את הרשימה להדפסת הלוח
     _size = 9
     board = []
     for i in range(_size):
@@ -7,6 +8,7 @@ def create_board():
     return board
 
 def print_board(_board):
+    #מדפיס את הרשימה בצורת לוח 3 על 3
     for i in range(0, 3, 6):
         print(f"{_board[i]} | {_board[i + 1]} | {_board[i + 2]}")
         print("----------")
@@ -25,13 +27,22 @@ def get_move(player, _board)
         else:
             continue
 
+def board_update():
+
+
+
 
 while True:
     _board = create_board()
-    print_board(_board)
 
-    get_move(player, _board)
-    board_update
-    resault_check
+    while True:
+        player = current_player
+        player_move = valid_input
+        player_input = ready_for_board
+        board_check(player_input)
+        board_update(player_input)
+        check_conditions = winner_tie
+        print_board(_board)
+        _end = check_end
 
-    start_stop
+_summery = resault_restart
